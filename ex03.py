ef79c7a4503b304db3b43f9728051d27ee34cf1c
slucaslimadev <lucaslima.rec@gmail.com>
2025-11-09 08:54:46 -0300

    exercicio 3-ok

---
+++ ex03.py
@@ -0,0 +1,24 @@
+print("--- CALCULOS DE SUBTRAÇÃO, ADIÇÃO, MULTIPLICAÇÃO, DIVISÃO")
+n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+
+#Soma
+soma = (int(n1) + int(n2))
+print("--- SOMA ---")
+print(f"Soma dos números: \nNúmero 1: {n1} \nNúmero 2: {n2} \nResultado: {soma}")
+
+#Subtração
+subtracao = (int(n1) - int(n2))
+print("--- SUBTRAÇÃO ---")
+print(f"Subtração dos números: \nNúmero 1: {n1} \nNúmero 2: {n2} \nResultado: {subtracao}")
+
+#multiplicação
+multiplicacao = (int(n1) * int(n2))
+print("--- Multiplicação ---")
+print(f"Multiplicação dos números: \nNúmero 1: {n1} \nNúmero 2: {n2} \nResultado: {multiplicacao}")
+
+#Divisão
+divisao = (int(n1) / int(n2))
+print("--- DIVISÃO ---")
+print(f"Multiplicação dos números: \nNúmero 1: {n1} \nNúmero 2: {n2} \nResultado: {divisao}")
+
